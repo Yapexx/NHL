eebdb341e60ee9600c801a83db744c3f3913bcff
Yannick Bruyère <Yannick Bruyre>
2017-08-21 19:38:59 +0200

    dummy line in database

--- src/bdd_connection/DummyLine.java
+++ src/bdd_connection/DummyLine.java
@@ -3,5 +3,11 @@ package bdd_connection;
 /**
  * Created by yannx_000 on 20/08/2017.
  */
+
+//To add empty line in all the table of the bdd
 public class DummyLine {
+
+    public static void insertPerfTable() {
+
+    }
 }
