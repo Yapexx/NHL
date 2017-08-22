24532147bd5005a7998d48a297b05d0b06a923f7
stagiaire3 <yannx.b@hotmail.fr>
2017-08-22 18:32:03 +0200

    ajout de toutes les équipes

--- src/bdd_connection/DummyLine.java
+++ src/bdd_connection/DummyLine.java
@@ -1,8 +1,31 @@
 package bdd_connection;
 
+import dao.PerfAwayTeamDAO;
+import dao.PerfHomeTeamDAO;
+import dao.StatsAwayMatchDAO;
+import dao.StatsHomeMatchDAO;
+import pojo.*;
+
 /**
  * Created by yannx_000 on 20/08/2017.
  */
 public class DummyLine {
 
+    public static void insertLines(){
+        for (Team team : Team.values()) {
+            PerfAwayTeam perfAwayTeam = new PerfAwayTeam(team ,0, 0, 0, 0, 0, 0, 0, 0, 0);
+            PerfHomeTeam perfHomeTeam = new PerfHomeTeam(team, 0,0,0,0,0,0,0,0,0);
+            StatsAwayMatch statsAwayMatch = new StatsAwayMatch(team, 0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+            StatsHomeMatch statsHomeMatch = new StatsHomeMatch(team, 0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+            PerfAwayTeamDAO perfAwayTeamDAO = new PerfAwayTeamDAO(SdzConnection.getInstance());
+            PerfHomeTeamDAO perfHomeTeamDAO = new PerfHomeTeamDAO(SdzConnection.getInstance());
+            StatsAwayMatchDAO statsAwayMatchDAO = new StatsAwayMatchDAO(SdzConnection.getInstance());
+            StatsHomeMatchDAO statsHomeMatchDAO = new StatsHomeMatchDAO(SdzConnection.getInstance());
+
+            perfAwayTeamDAO.create(perfAwayTeam);
+            perfHomeTeamDAO.create(perfHomeTeam);
+            statsHomeMatchDAO.create(statsHomeMatch);
+            statsAwayMatchDAO.create(statsAwayMatch);
+        }
+    }
 }
--- src/pojo/Team.java
+++ src/pojo/Team.java
@@ -14,7 +14,33 @@ public enum Team {
     Islanders(2, "New York Islanders"),
     Rangers(3, "New York Rangers"),
     Flyers(4, "Philadelphia Flyers"),
-    Penguins(5, "Pittsburgh Penguins");
+    Penguins(5, "Pittsburgh Penguins"),
+    Bruins(6, "Boston Bruins"),
+    Sabres(7, "Buffalo Sabres"),
+    Canadiens(8, "Montréal Canadiens"),
+    Senators(9, "Ottawa Senators"),
+    MapleLeafs(10, "Toronto Maple Leafs"), //deal with space
+    Hurricanes(12, "Carolina Hurricanes"),
+    Panthers(13, "Floraida Panthers"),
+    Ligthning(14, "Tampa Bay Lightning"),
+    Capitals(15, "Washington Capitals"),
+    Blackhawks(16, "Chicago Blackhawks"),
+    RedWings(17, "Detroit Red Wings"),
+    Predators(18, "Nashville Predators"),
+    Blues(19, "St. Louis Blues"),
+    Flames(20, "Calgary Flames"),
+    Avalanche(21, "Colorado Avalanche"),
+    Oilers(22, "Edmonton Oilers"),
+    Canucks(23, "Vancouver Canucks"),
+    Ducks(24, "Anaheim Ducks"),
+    Stars(25, "Dallas Stars"),
+    Kings(26, "Los Angeles Kings"),
+    Sharks(28, "San Jose Sharks"),
+    BlueJackets(29, "Colombus Blue Jackets"), //space
+    Wild(30, "Minnesota Wild"),
+    Jets(33, "Winnipeg Jets"),
+    Coyotes(53, "Arizona Coyotes"),
+    GoldenKnights(54, "Vegas Golden Knights"); //space
 
     /**
      * id of the team
